93025d27bee3c50948d5c12044d6ab95b4dce10d
Haley Hynes <haleyahynes@gmail.com>
2022-05-26 14:55:20 -0400

    Added Knowledge Check 1

---
+++ know_check_1.py
@@ -0,0 +1,14 @@
+# a statement that prints "hello world!"
+print('hello world!')
+
+# a list populated with several values. Print() one of those values to the prompt.
+lville_mayors = ['Harvey I. Sloane', 'Jerry Abramson', 'David L. Armstrong', 'Jerry Abramson', 'Greg Fischer']
+print(lville_mayors[4])
+
+# a dictionary populated with two keys and two values. Print one of the values.
+years_of_service = {'Harvey I. Sloane': '1982-1986', 'Jerry Abramson': '1986-1999', 'David L. Armstrong': '1999-2003', 'Jerry Abramson': '2003-2011', 'Greg Fischer': '2011-present'}
+print(years_of_service['Greg Fischer'])
+
+# a tuple with 4 values. Print one of them.
+party = ('Democrat', 'Democrat', 'Democrat', 'Democrat', 'Democrat')
+print(party[4])
